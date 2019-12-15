75483d068f903dd88f66956565c233ed04e1bdaa
Bryan Kegley <hi@bryankegley.me>
2019-12-14 19:27:45 -0600

    initial project setup

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
