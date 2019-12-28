c480bd21989efe076db640604e68b63fc9e6a7c6
Bryan Kegley <hi@bryankegley.me>
2019-12-28 13:11:06 -0600

    fix loader types

--- packages/graphql-utils/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-// export type LoaderFn<T> = (ids: T extends any[] ? string[][] : string[], models: any) => Promise<Array<T>>
-export type LoaderFn<T> = (ids: unknown[], models: any) => Promise<Array<T>>
--- packages/graphql-utils/src/index.ts
+++ packages/graphql-utils/src/index.ts
@@ -1,3 +1,12 @@
+import {BatchLoadFn, default as Dataloader} from 'dataloader'
+
+export type LoaderFn<T> = (ids: string[], models: any) => ReturnType<BatchLoadFn<string, T>>
+type ExtractGraphQLType<L> = L extends LoaderFn<infer T> ? T : never
+
+export type LoaderContext<L> = {
+  [K in keyof L]: Dataloader<string, ExtractGraphQLType<L[K]>>
+}
+
 export {createConnectionResults} from './createConnectionResults'
 export {createCursorHash, parseCursorHash} from './cursor'
 export {parseArgs} from './parseArgs'
