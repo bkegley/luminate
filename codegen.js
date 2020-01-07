d39efed561c122782299891f81a7fbaef1050d86
Bryan Kegley <hi@bryankegley.me>
2020-01-07 13:36:26 -0600

    update codegen

--- codegen.js
+++ codegen.js
@@ -11,6 +11,18 @@ const clientGenerates = {
       federation: true,
     },
   },
+  'packages/gatsby-theme-luminate/src/graphql/index.tsx': {
+    documents: 'packages/gatsby-theme-luminate/src/graphql/**/*.graphql',
+    schema: 'http://localhost:3000/graphql',
+    plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+    config: {
+      withComponent: false,
+      withHOC: false,
+      withHooks: true,
+      nonOptionalTypename: true,
+      federation: true,
+    },
+  },
 }
 
 const serverGenerates = {
