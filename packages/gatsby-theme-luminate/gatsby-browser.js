751514ce4765bc87dcf99ee3e400b0d5ae341623
Bryan Kegley <hi@bryankegley.me>
2020-01-09 13:17:00 -0600

    add theme-ui to luminate-theme

--- packages/gatsby-theme-luminate/gatsby-browser.js
+++ packages/gatsby-theme-luminate/gatsby-browser.js
@@ -2,14 +2,10 @@ import React from 'react'
 import {UserProvider} from './src/context'
 import {ApolloProvider} from '@apollo/react-hooks'
 import createClient from './src/createClient'
-import {ThemeProvider} from 'theme-ui'
-import theme from './src/theme'
 
 export const wrapRootElement = ({element}, {authWrapper = true, uri = 'http://localhost:3000/graphql'}) => {
   const client = createClient({uri})
   return (
-    <ThemeProvider theme={theme}>
-      <ApolloProvider client={client}>{authWrapper ? <UserProvider>{element}</UserProvider> : element}</ApolloProvider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>{authWrapper ? <UserProvider>{element}</UserProvider> : element}</ApolloProvider>
   )
 }
