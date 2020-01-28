1e59731f00fd29b812418db50033c563eac97fdf
Bryan Kegley <hi@bryankegley.me>
2020-01-28 07:45:05 -0600

    add Drawer

--- packages/gatsby-theme-luminate/src/components/index.tsx
+++ packages/gatsby-theme-luminate/src/components/index.tsx
@@ -1,5 +1,7 @@
 export * from '@theme-ui/components'
 import Select, {SelectProps} from './Select'
 import Combobox, {ComboboxProps} from './Combobox'
+import Drawer from './Drawer'
+export {Drawer}
 
 export {Combobox, ComboboxProps, Select, SelectProps}
