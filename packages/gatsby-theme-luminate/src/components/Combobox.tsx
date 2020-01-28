fc69bd9a2783c622b809c51ce2cdbea5fea443e9
Bryan Kegley <hi@bryankegley.me>
2020-01-27 20:36:45 -0600

    add formik support to combobox
    Co-authored-by: Tony Nguyen <tonynguyen201@gmail.com>

--- packages/gatsby-theme-luminate/src/components/Combobox.tsx
+++ packages/gatsby-theme-luminate/src/components/Combobox.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import {jsx} from 'theme-ui'
 import React from 'react'
-import {useCombobox} from 'downshift'
+import {useCombobox, UseComboboxState} from 'downshift'
 import defaultStyles, {IStyles} from './styles'
 import {Box, Button, Label, Input} from '@theme-ui/components'
 import DownArrow from './DownArrow'
@@ -13,12 +13,13 @@ interface IItem {
 
 export interface ComboboxProps {
   label: React.ReactNode
-  onCreateNew?: Function
+  onChange?: (values: Partial<UseComboboxState<IItem>>) => void
+  onCreateNew?: (values: Partial<UseComboboxState<IItem>>) => void
   options: IItem[]
   styles?: IStyles
 }
 
-const Combobox = ({options, label, onCreateNew, styles = defaultStyles}: ComboboxProps) => {
+const Combobox = ({options, label, onChange, onCreateNew, styles = defaultStyles}: ComboboxProps) => {
   const itemToString = (option: IItem | null) => (option ? option.name : '')
   const createNewOptions: IItem[] = onCreateNew ? [{name: '-- Create New --', value: '__createNew'}] : []
   const initialOptions = createNewOptions.concat(options)
@@ -51,9 +52,11 @@ const Combobox = ({options, label, onCreateNew, styles = defaultStyles}: Combobo
     },
     itemToString,
     onSelectedItemChange: changes => {
-      console.log({changes})
       if (onCreateNew && changes.selectedItem?.value === '__createNew') {
         onCreateNew(changes)
+      }
+      if (onChange && changes.selectedItem?.value !== '__createNew') {
+        onChange(changes)
       }
     },
   })
@@ -95,30 +98,7 @@ const Combobox = ({options, label, onCreateNew, styles = defaultStyles}: Combobo
           })}
         </ul>
       ) : null}
-      {/* <label {...getLabelProps()}>Choose an element:</label>
-      <div style={{ display: "inline-block" }} {...getComboboxProps()}>
-        <input {...getInputProps()} />
-        <button {...getToggleButtonProps()} aria-label="toggle menu">
-          &#8595;
-        </button>
-      </div>
-      <ul {...getMenuProps()} style={menuStyles}>
-        {isOpen &&
-          inputOptions.map((item, index) => (
-            <li
-              style={
-                highlightedIndex === index ? { backgroundColor: "#bde4ff" } : {}
-              }
-              key={`${item}${index}`}
-              {...getItemProps({ item, index })}
-            >
-              {item}
-            </li>
-          ))}
-      </ul> */}
     </Box>
-    // <>
-    // </>
   )
 }
 
