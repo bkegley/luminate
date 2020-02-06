9612e7d262a3cde3a03b2fb323b1b4a61ef453c2
Bryan Kegley <hi@bryankegley.me>
2020-02-05 23:45:58 -0600

    add Modal

--- packages/gatsby-theme-luminate/src/components/index.tsx
+++ packages/gatsby-theme-luminate/src/components/index.tsx
@@ -1,7 +1,8 @@
 export * from '@theme-ui/components'
 import Combobox, {ComboboxProps} from './Combobox'
+import Modal, {ModalProps} from './Modal'
 import Select, {SelectProps} from './Select'
 import Tooltip, {TooltipProps} from './Tooltip'
 import Drawer from './Drawer'
 
-export {Combobox, ComboboxProps, Drawer, Select, SelectProps, Tooltip, TooltipProps}
+export {Combobox, ComboboxProps, Drawer, Modal, ModalProps, Select, SelectProps, Tooltip, TooltipProps}
