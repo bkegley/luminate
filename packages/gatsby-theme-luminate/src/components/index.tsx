24268d7f3fb31d9705f1a72d68802944811563c3
Bryan Kegley <hi@bryankegley.me>
2020-02-09 21:34:37 -0600

    add StyledLink

--- packages/gatsby-theme-luminate/src/components/index.tsx
+++ packages/gatsby-theme-luminate/src/components/index.tsx
@@ -2,6 +2,7 @@ import Combobox, {ComboboxProps} from './Combobox'
 import Layout, {LayoutProps} from './Layout'
 import Modal, {ModalProps} from './Modal'
 import Select, {SelectProps} from './Select'
+import StyledLink, {StyledLinkProps} from './StyledLink'
 import Tooltip, {TooltipProps} from './Tooltip'
 import Drawer from './Drawer'
 
@@ -15,6 +16,8 @@ export {
   ModalProps,
   Select,
   SelectProps,
+  StyledLink,
+  StyledLinkProps,
   Tooltip,
   TooltipProps,
 }
