b9ec1d829094c620bd3fe98de79bf1c26cd1efad
Bryan Kegley <hi@bryankegley.me>
2020-02-20 23:29:58 -0600

    refactor modal

--- packages/app/src/components/RootHeader.tsx
+++ packages/app/src/components/RootHeader.tsx
@@ -1,50 +1,54 @@
 /** @jsx jsx */
 import {jsx, Flex, Box, Heading, Button} from 'theme-ui'
-import {useUser, UserFragmentFragment, Modal} from '@luminate/gatsby-theme-luminate/src'
+import {useUser, Modal, useDialogState, DialogDisclosure} from '@luminate/gatsby-theme-luminate/src'
 import {Link} from 'gatsby'
 import LoginForm from './LoginForm'
 
 interface HeaderProps {}
 
 export const Header = ({}: HeaderProps) => {
-  const {data, logout} = useUser()
+  const {user, logout} = useUser()
+  const loginDialog = useDialogState()
   return (
     <Box sx={{position: 'fixed', width: '100%', top: 0}}>
-      <Flex sx={{width: '100%', alignItems: 'center', justifyContent: 'space-between', py: 2}}>
+      <Modal dialog={loginDialog} aria-label="Login">
+        <Box
+          sx={{
+            width: ['90vw', '75vw', '50vw'],
+            maxWidth: '600px',
+          }}
+        >
+          <LoginForm />
+        </Box>
+      </Modal>
+      <Flex
+        sx={{
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          py: 2,
+          maxWidth: 'contentWidth',
+          mx: 'auto',
+        }}
+      >
         <Box>
-          <Link to="/">
-            <Heading>Luminate</Heading>
+          <Link to="/app" sx={{textDecoration: 'none'}}>
+            <Heading as="h3" sx={{color: 'primary'}}>
+              Luminate
+            </Heading>
           </Link>
         </Box>
         <Flex sx={{alignItems: 'center'}}>
-          <Box sx={{mx: 2}}>{data ? <Link to="/app">Go to App</Link> : <Link to="/register">Register</Link>}</Box>
+          <Box sx={{mx: 2}}>{user ? <Link to="/app">Go to App</Link> : <Link to="/register">Register</Link>}</Box>
           <Box sx={{ml: 2}}>
-            {data ? (
-              <Button type="button" onClick={logout} variant="text">
+            {user ? (
+              <Button type="button" onClick={() => logout()} variant="text">
                 Logout
               </Button>
             ) : (
-              <Modal
-                disclosure={
-                  <Button type="button" variant="outline">
-                    Login
-                  </Button>
-                }
-                aria-label="Login"
-              >
-                {dialog => {
-                  return (
-                    <Box
-                      sx={{
-                        width: ['90vw', '75vw', '50vw'],
-                        maxWidth: '600px',
-                      }}
-                    >
-                      <LoginForm />
-                    </Box>
-                  )
-                }}
-              </Modal>
+              <DialogDisclosure {...loginDialog} as={Button} variant="outline">
+                Login
+              </DialogDisclosure>
             )}
           </Box>
         </Flex>
