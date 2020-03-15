888594ffad1421d7a77fae70f5f1ce0092e469f0
Bryan Kegley <hi@bryankegley.me>
2020-03-14 23:39:46 -0500

    add Heading and StyleLink

--- packages/gatsby-theme-luminate/src/components/StyledLink.tsx
+++ packages/gatsby-theme-luminate/src/components/StyledLink.tsx
@@ -3,21 +3,14 @@ import {jsx} from 'theme-ui'
 import {Link, LinkProps, useRouteMatch} from 'react-router-dom'
 
 export interface StyledLinkProps extends LinkProps {
-  activeStyle?: object
+  activeClassName?: string
   activeOnlyWhenExact?: boolean
-  variant?: string
 }
 
-const StyledLink = ({activeOnlyWhenExact, activeStyle = {color: 'primary'}, variant, ...props}: StyledLinkProps) => {
+const StyledLink = ({activeOnlyWhenExact, activeClassName = 'text-primary-600', ...props}: StyledLinkProps) => {
   const {to} = props
   const match = useRouteMatch({path: to as string, exact: activeOnlyWhenExact})
-  return (
-    <Link
-      {...props}
-      className={match ? 'active' : ''}
-      sx={{textDecoration: 'none', '&.active': activeStyle, variant: variant}}
-    />
-  )
+  return <Link {...props} className={`no-underline ${match ? activeClassName : ''}`} />
 }
 
 export default StyledLink
