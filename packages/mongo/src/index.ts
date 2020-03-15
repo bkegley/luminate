7ee711a3d86f216ded5bc1b0188b49c6e6d639ad
Bryan Kegley <hi@bryankegley.me>
2020-03-15 15:27:32 -0500

    seed database on startup

--- packages/mongo/src/index.ts
+++ packages/mongo/src/index.ts
@@ -1,5 +1,6 @@
 import createMongoConnection from './createMongoConnection'
+import seedDatabase from './seedDatabase'
 export * from './models'
 export {WithAuthenticatedMethods} from './baseSchemas'
 
-export {createMongoConnection}
+export {createMongoConnection, seedDatabase}
