1ad7495976147d438bba7b2a4c5fa4a4aefb3646
Bryan Kegley <hi@bryankegley.me>
2020-09-13 21:29:56 -0500

    Add test for failure to create already existing role
    Co-authored-by: Andrew Lane <contact@lungers.com>

--- packages/backend/services/identity/schema/src/commands/CreateRoleCommandHandler.ts
+++ packages/backend/services/identity/schema/src/commands/CreateRoleCommandHandler.ts
@@ -2,18 +2,33 @@ import {Producer} from 'kafka-node'
 import {CreateRoleCommand, ICommandHandler} from '.'
 import {RoleDocument, RoleModel} from '../models'
 import {RoleCreatedEvent} from '../events'
+import {IRolesAggregate} from '../aggregates'
 
 type CreateRoleResponse = RoleDocument
 
 export class CreateRoleCommandHandler implements ICommandHandler<CreateRoleCommand, CreateRoleResponse> {
   private producer: Producer
+  private rolesAggregate: IRolesAggregate
 
-  constructor(producer: Producer) {
+  constructor(producer: Producer, rolesAggregate: IRolesAggregate) {
     this.producer = producer
+    this.rolesAggregate = rolesAggregate
   }
 
   public async handle(command: CreateRoleCommand) {
-    const role = new RoleModel(command)
+    const role = new RoleModel({
+      name: command.name,
+      scopes: command.scopes,
+      readAccess: [command.account],
+      writeAccess: [command.account],
+      adminAccess: [command.account],
+    })
+
+    const existingRole = await this.rolesAggregate.getRoleByName(command.name).catch(err => {})
+
+    if (existingRole) {
+      throw new Error('Role name taken')
+    }
 
     const roleCreatedEvent = new RoleCreatedEvent(role)
 
