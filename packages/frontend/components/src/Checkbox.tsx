e6afde5082e43498389185015a9edb0b2f049a4f
Bryan Kegley <hi@bryankegley.me>
2021-02-17 16:54:23 -0600

    fix checkbox checked styling in dark mode

--- packages/frontend/components/src/Checkbox.tsx
+++ packages/frontend/components/src/Checkbox.tsx
@@ -13,7 +13,7 @@ export const Checkbox = (props: CheckboxProps) => {
   return (
     <input
       type="checkbox"
-      className="focus:ring-primary-200 h-4 w-4 text-primary-400 border-gray-300 rounded"
+      className="focus:ring-primary-200 h-4 w-4 dark:bg-gray-800 text-primary-400 checked:bg-primary-400 dark:checked:bg-primary-400 border-gray-300 dark:border-gray-700 rounded"
       {...props}
     />
   );
