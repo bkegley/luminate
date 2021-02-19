c4fb499fe77cc737c0a18e30ace0fa24738c2b2b
Bryan Kegley <hi@bryankegley.me>
2021-02-19 15:31:31 -0600

    remove extra components

--- packages/frontend/gatsby-theme-luminate/src/components/index.tsx
+++ packages/frontend/gatsby-theme-luminate/src/components/index.tsx
@@ -1,43 +1,4 @@
-import Avatar, {AvatarProps} from './Avatar'
-import Button, {ButtonProps} from './Button'
-import Card, {CardProps} from './Card'
-import Combobox, {ComboboxProps} from './Combobox'
-import Heading, {HeadingProps} from './Heading'
-import Input, {InputProps} from './Input'
-import Menu, {MenuProps} from './Menu'
-export {MenuArrow, MenuBar, MenuItem, MenuGroup, MenuSeparator} from 'reakit/Menu'
 import Modal, {ModalProps} from './Modal'
 export {DialogDisclosure, useDialogState} from 'reakit/Dialog'
-import Select, {SelectProps} from './Select'
-import Spinner, {SpinnerProps} from './Spinner'
-import StyledLink, {StyledLinkProps} from './StyledLink'
-import Tooltip, {TooltipProps} from './Tooltip'
-import Drawer from './Drawer'
 
-export {
-  Avatar,
-  AvatarProps,
-  Button,
-  ButtonProps,
-  Card,
-  CardProps,
-  Combobox,
-  ComboboxProps,
-  Drawer,
-  Heading,
-  HeadingProps,
-  Input,
-  InputProps,
-  Menu,
-  MenuProps,
-  Modal,
-  ModalProps,
-  Select,
-  SelectProps,
-  Spinner,
-  SpinnerProps,
-  StyledLink,
-  StyledLinkProps,
-  Tooltip,
-  TooltipProps,
-}
+export {Modal, ModalProps}
