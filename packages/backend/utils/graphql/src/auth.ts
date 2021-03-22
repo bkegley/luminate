8032672aff03bb7632b9570ef68e05e45e38ff98
Bryan Kegley <hi@bryankegley.me>
2021-03-21 20:49:45 -0500

    add authentication decorators to graphql-utils

--- packages/backend/utils/graphql/src/auth.ts
+++ packages/backend/utils/graphql/src/auth.ts
@@ -1,7 +1,5 @@
-import {AuthenticationError, ForbiddenError} from 'apollo-server-express'
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import {scopes} from '@luminate/mongo-utils'
 
 interface TokenInput {
   jti: string
@@ -26,21 +24,6 @@ export interface Token extends TokenInput {
   exp: number
 }
 
-// const createToken = (res: express.Response, input: TokenInput, secret: string) => {
-//   const token = jwt.sign(input, secret, {expiresIn: '15m'})
-//   res.cookie('id', token, {
-//     httpOnly: false,
-//     secure: process.env.NODE_ENV === 'production',
-//   })
-//   return token
-// }
-
-// const removeToken = (res: express.Response): void => {
-//   res.cookie('id', '', {
-//     expires: new Date(0),
-//   })
-// }
-
 export const parseTokenFromRequest = (req: express.Request, secret: string) => {
   if (!req.headers.authorization) return null
   const [bearer, token] = req.headers.authorization.split(' ')
@@ -55,39 +38,4 @@ export const parseUserFromRequest = (request: express.Request): Token | null => 
   const authHeader = (request.headers['x-auth-user'] as string) || null
   const user = authHeader ? JSON.parse(Buffer.from(authHeader, 'base64').toString('utf-8')) : null
   return user
-}
-
-export const hasRole = (user: Token | null, roleName: string) => {
-  return new Promise<void>((resolve, reject) => {
-    if (!user) {
-      reject(new AuthenticationError('Please authenticate'))
-      return
-    }
-    const roles = (user.roles as unknown) as Array<{name: string; scopes: string[]}> | undefined
-    const hasRole = !!roles?.find(role => role.name === roleName)
-    if (!hasRole) {
-      reject(new ForbiddenError('You do not have permission to view this resource'))
-      return
-    }
-    resolve()
-  })
-}
-
-type ValidScopes = keyof typeof scopes
-
-export const hasScopes = (user: Token | null, requiredScopes: ValidScopes[]) => {
-  return new Promise<void>((resolve, reject) => {
-    if (!user) {
-      reject(new AuthenticationError('Please authenticate'))
-      return
-    }
-
-    const hasScopes = requiredScopes.every(requiredScope => !!user.scopes?.find(scope => scope === requiredScope))
-
-    if (!hasScopes) {
-      reject(new ForbiddenError('You do not have permission to view this resource'))
-      return
-    }
-    resolve()
-  })
 }
