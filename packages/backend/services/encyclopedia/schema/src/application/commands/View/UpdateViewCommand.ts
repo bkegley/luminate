1ec822f0b9c82e642a705185a3a5fc77786a941f
Bryan Kegley <hi@bryankegley.me>
2021-04-18 15:57:00 -0500

    fix: update view works

--- packages/backend/services/encyclopedia/schema/src/application/commands/View/UpdateViewCommand.ts
+++ packages/backend/services/encyclopedia/schema/src/application/commands/View/UpdateViewCommand.ts
@@ -4,9 +4,11 @@ import {UpdateViewInput} from '../../../types'
 export class UpdateViewCommand {
   name?: string
   description?: string
+  jsonString?: string
 
   constructor(public user: Token, public id: string, input: UpdateViewInput) {
     this.name = input.name
     this.description = input.description
+    this.jsonString = input.jsonString
   }
 }
